1d6ffe6977cb1deb58f33af731e20269673f51cc
Dan O'Shea <6539132+uXeBoy@users.noreply.github.com>
2020-02-15 14:02:32 +1030

    Convert .hex files to .bin

--- docs/updater/config-template.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configuration
-$server = 'https://raw.githubusercontent.com/';
-$repopath = 'eried/ArduboyCollection/'; // Source repository
-
-// Github credentials
-$client_id='get from github';
-$client_secret='get from github';
